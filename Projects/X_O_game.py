f24bff5f1640e1e97a75d343ab4a79d2f239668f
KaiserMusa <kzoz@tutanota.com>
2020-10-16 21:52:09 +0900

    Tic tac toe game

---
+++ Projects/X_O_game.py
@@ -0,0 +1,7 @@
+# 9 empty grids wiht background calculation to do (timer of 10 seconds)
+# If the user doesn't solve his/her problem wihtin the time, user eiehr loose or pass his/her turn
+#  take inputs from user and place it in the adequate grid, they want it to be at
+#   make the pc counter attack the user's strategy to align 3 X or 3 O
+#  the first one aligning 3 symbols in a row wins
+
+
