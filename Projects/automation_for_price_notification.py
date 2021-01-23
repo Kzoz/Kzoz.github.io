352c663859f5388b2ef4dde8b0ce2a1a1b050b8d
KaiserMusa <kzoz@tutanota.com>
2021-01-23 20:55:13 +0900

    minor changes

--- Projects/automation_for_price_notification.py
+++ Projects/automation_for_price_notification.py
@@ -3,9 +3,14 @@ from bs4 import BeautifulSoup
 import smtplib
 import time
 
+#Define the source of the research (or the website from which the info will be picked up from)
 URL = 'https://kakaku.com/item/J0000033539/' # Sony XM4
 # URL = 'https://kakaku.com/item/K0001206017/' #Airpods Pro
 headers= {"User-Agent":'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.2 Safari/605.1.15'}
+
+# Check price: Using Requests, 
+# find which class or ID hold the title and price of hte item we want to track.
+#Convert the price(str) into num
 
 def check_price():
 
@@ -20,6 +25,7 @@ def check_price():
         send_mail()
     print(title, conv_price)
 
+#Send the info via email
 def send_mail():
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.ehlo()
