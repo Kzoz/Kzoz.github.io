bf356d96bc4570dfce962a3e2dbeacf6b155b6a5
KaiserMusa <kzoz@tutanota.com>
2021-02-02 22:59:10 +0900

    minor changes

--- python/test_whatever.py
+++ python/test_whatever.py
@@ -1,17 +1,42 @@
-def binarysearch(lst, index):
-    begin_index = 0
-    end_index = len(lst)-1
+# set arr 
+#set i in arr where is < arr[0] and > arr[len(arr)-1]
+#
 
-    while begin_index <= end_index:
-        mid_index = begin_index + (end_index-begin_index)//2
-        mid_val = lst[mid_index]
-        if mid_val == index:
-            return mid_index 
-        elif index < mid_val:
-            end_index = mid_index-1
-        else:
-            begin_index = mid_index+1
-    return None
+#def mountainarray(arr):
 
-lst_a = [1,2,3,4,5,6,7,8,9,10,11]
-print(binarysearch(lst_a, 8))
+#    cnt = 0
+#    while len(arr) > 2:
+#        for i in range(1,len(arr)-1):
+#            if arr[0] >= arr[1] or arr[-1] >= arr[-2]:
+#                return False
+#            if arr[i] > arr[0] and arr[i] > arr[-1]:
+#                cnt += 1
+#        if cnt == (len(arr)-2):
+#            return True
+        
+#        else:
+#            return False
+        
+
+#print(mountainarray([3,5,5])) #ppp = [3,5,5]
+
+
+
+def mountainvalid(arr):
+    mountain = True
+    if len(arr) < 3:
+        return False
+    if arr[0] > arr[1]:
+        return False
+    if arr[-1] > arr[-2]:
+        return False
+    for i in range(len(arr) - 1):
+        if arr[i] >= arr[i + 1]:
+            mountain = False
+        if not mountain and arr[i] <= arr[i + 1]:
+            return False
+    return True
+
+    
+print(mountainvalid([5,6,7,1]))
+
