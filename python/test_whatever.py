727e363c2320aeed3033385947bece5909b35a5f
KaiserMusa <kzoz@tutanota.com>
2021-02-09 10:08:12 +0900

    minor changes

--- python/test_whatever.py
+++ python/test_whatever.py
@@ -1,42 +1,16 @@
-# set arr 
-#set i in arr where is < arr[0] and > arr[len(arr)-1]
-#
+def binary_search(lst, elem):
+    start_point = 0
+    end_point = len(lst)-1
+    while start_point <= end_point:
+        mid_index = start_point + (end_point - start_point)//2
+        mid_val = lst[mid_index]
+        if mid_val == elem:
+            return mid_index
+        elif elem < mid_val:
+            end_point = mid_index-1
+        else:
+            start_point = mid_index+1
+    return None
 
-#def mountainarray(arr):
-
-#    cnt = 0
-#    while len(arr) > 2:
-#        for i in range(1,len(arr)-1):
-#            if arr[0] >= arr[1] or arr[-1] >= arr[-2]:
-#                return False
-#            if arr[i] > arr[0] and arr[i] > arr[-1]:
-#                cnt += 1
-#        if cnt == (len(arr)-2):
-#            return True
-        
-#        else:
-#            return False
-        
-
-#print(mountainarray([3,5,5])) #ppp = [3,5,5]
-
-
-
-def mountainvalid(arr):
-    mountain = True
-    if len(arr) < 3:
-        return False
-    if arr[0] > arr[1]:
-        return False
-    if arr[-1] > arr[-2]:
-        return False
-    for i in range(len(arr) - 1):
-        if arr[i] >= arr[i + 1]:
-            mountain = False
-        if not mountain and arr[i] <= arr[i + 1]:
-            return False
-    return True
-
-    
-print(mountainvalid([5,6,7,1]))
-
+lst_a = [1,2,3,4,5,6,7,8,9,10]
+print(binary_search(lst_a,4))
