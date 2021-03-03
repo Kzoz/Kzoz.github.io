a2c4ed0e670704eedbf7793089a5ec5bc6af3679
KaiserMusa <kzoz@tutanota.com>
2021-03-03 11:42:31 +0900

    new python exos

--- python/test_whatever.py
+++ python/test_whatever.py
@@ -1,6 +1,11 @@
-nums = [1,2,3,4,5,6,7]
-def yielding(lst):
-    for i,j in enumerate(lst):
-        yield i
-        #return j
-print(yielding(nums))
+def insertion_sort(lst):
+    sequence = range(1,len(lst))
+    for i in sequence:
+        toSort = lst[i]
+        while lst[i-1] > toSort and i > 0:
+            lst[i-1], lst[i] = lst[i], lst[i-1]
+            i -=1
+    return lst
+
+lst_a = [2,3,7,5,4]
+print(insertion_sort(lst_a))
