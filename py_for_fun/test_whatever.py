4d015c4f68dbe479589fcf781e14d6a4e2a57d21
KaiserMusa <kzoz@tutanota.com>
2022-03-04 09:16:57 +0900

    minor modif

---
+++ py_for_fun/test_whatever.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+root = Tk()
+
+#myLabel1 = Label(root, text="hi!")
+#myLabel2 = label(root, text= "hello!")
+
+#myLabel1.grid(row=0,column=0)
+#myLabel2.grid(row=1,column=0)
+myButton = Button(root, text="Click Me!")
+myButton.pack()
+
+root.mainloop()
