76161acedbf25419d56f8cd5eddaaa2cefc1289a
kzoz <kzoz@tuta.io>
2022-10-06 16:12:29 +0900

    python automation

--- PROJECTS/py3_automata/automation_for_price_notification.py
+++ PROJECTS/py3_automata/automation_for_price_notification.py
@@ -1,12 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
 import smtplib
-import time
 
 #Define the source of the research (or the website from which the info will be picked up from)
-URL = 'https://kakaku.com/item/J0000033539/' # Sony XM4
+#URL = 'https://kakaku.com/item/J0000033539/' # Sony XM4
 # URL = 'https://kakaku.com/item/K0001206017/' #Airpods Pro
-headers= {"User-Agent":'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.2 Safari/605.1.15'}
+URL = 'https://www.amazon.co.jp/gp/product/B08JFTKHF2/ref=ox_sc_saved_title_1?smid=A2A0GDJVUPL7UQ&psc=1' #For Protein
+
+headers= {"User-Agent":'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36'}
 
 # Check price: Using Requests, 
 # find which class or ID hold the title and price of the item we want to track.
@@ -17,11 +18,11 @@ def check_price():
     page = requests.get(URL,  headers=headers)
     soup= BeautifulSoup(page.content, 'html.parser')
     #print(soup.prettify())
-    title = soup.find(attrs={'newBrand'}).get_text()
-    price = soup.find(attrs={'priceTxt'}).get_text()
-    conv_price = float(price[1:7].replace(',',''))
+    title = soup.find(id={'productTitle'}).get_text()
+    price = soup.find(id={'sns-base-price'}).get_text()
+    conv_price = float(price.strip()[1:6].replace(',',''))
 
-    if(conv_price < 32000):
+    if(conv_price < 3500):
         send_mail()
     print(title, conv_price)
 
@@ -32,16 +33,15 @@ def send_mail():
     server.starttls()
     server.ehlo()
     
-    server.login('XYZ', 'XYZ')
-    subject = 'Sony XM4 price fell down'
-    body = 'ssCheck the Kakaku link: https://kakaku.com/item/J0000033539/'
+    server.login('username', 'pwd')
+    subject = 'PROTEIN PRICE IS DOWN'
+    body = " Check the Amazon link: 'https://www.amazon.co.jp/gp/product/B08JFTKHF2/ref=ox_sc_saved_title_1?smid=A2A0GDJVUPL7UQ&psc=1' "
     
     msg = f"Subject: {subject} \n\n {body}"
     
-    server.sendmail('d.pape.moussa@gmail.com', 'd.pape.moussa@gmail.com', msg)
-    print('Hey the price reduction email has been sent!!')
+    server.sendmail('username', 'username', msg)
+    print('Hey the price of X has gone down')
     server.quit()
     
 
 check_price()
-#time.sleep(604800)
